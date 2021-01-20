96a54690dd8af0d8feea0fa6838074faa8b9be1e
Artem <artem@gmail.com>
2021-01-20 15:58:39 +0200

    Added code for register_server and get_servers functions

--- cloud_server.py
+++ cloud_server.py
@@ -1,6 +1,7 @@
 # python -m flask run
+import json
 import sqlite3
-from flask import Flask, request, g
+from flask import Flask, request, g, Response
 
 app = Flask(__name__)
 DATABASE = 'servers.db'
@@ -11,7 +12,7 @@ def get_db():
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
-@app.teardown_appcontext
+@app.teardown_request
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
@@ -19,31 +20,44 @@ def close_connection(exception):
 
 @app.route('/delete_server',methods=['DELETE'])
 def delete_server():
-    name = request.form.get('name')
-    ip = request.form.get('ip')
-    port = request.form.get('port')
-    number_players = request.form.get('number_players')
-    max_players = request.form.get('max_players')
+    pass
 
 @app.route('/connect_to_server',methods=['PUT'])
 def connect_to_server():
     name = request.form.get('name')
     ip = request.form.get('ip')
     port = request.form.get('port')
-    number_players = request.form.get('number_players')
+    number_players = request.form.get('players')
     max_players = request.form.get('max_players')
 
-@app.route('/register_server',methods=['POST'])
+@app.route('/register_server',methods=['GET'])
 def register_server():
     name = request.form.get('name')
     ip = request.form.get('ip')
     port = request.form.get('port')
-    number_players = request.form.get('number_players')
+    number_players = request.form.get('players')
     max_players = request.form.get('max_players')
+    db = get_db()
+    try:
+        db.execute('insert into available_servers values (?,?,?,?,?)',(name,ip,port,number_players,max_players))
+        db.commit()
+    except sqlite3.IntegrityError:
+        res = app.make_response('Server name is not unique')
+        res.status_code = 400
+        return res
+    except Exception as e:
+        res = app.make_response(str(e))
+        res.status_code = 500
+    return res
 
 @app.route('/get_servers',methods=['GET'])
 def get_servers():
-    pass
+    db = get_db()
+    cursor = db.execute('select * from available_servers')
+    columns = [item[0] for item in cursor.description]
+    raw_data = cursor.fetchall()
+    data = [dict(zip(columns,row)) for row in raw_data]
+    return Response(json.dumps(data, default=lambda x: x.__dict__))
 
 @app.route('/',methods=['GET'])
 def root():
