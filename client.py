054d3dbb82305708c7dc7025ed86735514e30138
Artem <artem@gmail.com>
2021-01-27 17:01:29 +0200

    Changed class Client

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 HOST = 'localhost'
 PORT = 65432
@@ -7,14 +8,33 @@ class Client:
     def __init__(self,host,port):
         self.host = host
         self.port = port
+        self.__socket = None
+        self.__close_msg = b'closing connection'
 
-    def send(self,data):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((self.host, self.port))
-            s.sendall(data)
-            response = s.recv(1024)
-        return response
+    def connect(self):
+        if self.__socket is not None:
+            self.close()
+        else:
+            self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.__socket.connect((self.host, self.port))
 
-client = Client(HOST,PORT)
-res = client.send(b'Hello network!')
-print(res)
+    def send(self, data):
+        if data == self.__close_msg:
+            raise Exception('It\'s reserved message')
+        elif self.__socket is None:
+            raise Exception('The client has no connection')
+        else:
+            self.__socket.sendall(data)
+            response = self.__socket.recv(1024)
+            return response
+
+    def close(self):
+        self.__socket.sendall(self.__close_msg)
+        self.__socket.close()
+
+
+if __name__ == '__main__':
+    for i in range(1):
+        print(i)
+        client = Client(HOST,PORT)
+        print(client.send(b'Hello network'+bytes(str(i),'utf-8')))
